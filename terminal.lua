b796dae94b1971afd9a063e52cc5ab7ad60af36f
Andrew Cannon <ajc@gmx.net>
2019-07-12 09:26:25 +1000

    WIP line wrapping (explicit)

--- terminal.lua
+++ terminal.lua
@@ -1,22 +1,32 @@
 --%tabs=3
 --[[
 
-		display terminal for love2d
+	Display Terminal for love2d
 		
+	Copyright(c) 2019 Andrew Cannon <ajc@gmx.net>
+	
+	Licensed under the MIT licence.
+	For details see file LICENSE in this directory.
+
 --]]
 
 require "dump"
 
-local ipairs = ipairs
 
 local string = require "string"
 local table = require "table"
-local love = require "love"
 local math = require "math"
+local utf8 = require "utf8"
+local love = require "love"
 
-local dump = dump
 local tostring = tostring
 local unpack = unpack or table.unpack
+local ipairs = ipairs
+local assert = assert or function(b) if not b then error"assertion" end end
+local error = error
+local pcall = pcall
+local dump = dump
+
 
 local terminal = {}
 if _VERSION:match"Lua 5%.[12]" then
@@ -24,85 +34,118 @@ if _VERSION:match"Lua 5%.[12]" then
 	module "terminal"
 end
 
-local g_screen = {{ text = "", num = 1 }}
-local g_scroll
-local g_lmarg, g_rmarg = 2, 35
+local max = math.max
+local min = math.min
+local abs = math.abs
+local ceil = math.ceil
+local floor = math.floor
+local round = function(x) return floor(x + 0.5) end
 
+local insert = table.insert
+local remove = table.remove
+local concat = table.concat
+
+local format = string.format
+local errorf = function(...) error(format(...)) end
+
+-- config
+local g_lmarg, g_rmarg = 2, 35
+local g_idgap, g_trgap = 2, 2
+local g_maxblocks = 10000
+
+-- screen parameters
 local g_width, g_height
 local g_font
 local g_lineHeight
---local g_dpiScale
-local g_textw, g_texth
 local g_nlines
 
-local g_maxblocks = 10000
-local g_blocknum = 0
+-- display contents
+local g_screen
+local g_scroll
+local g_lineref
+local g_linecount
+local g_blockid
+
 
 -- compute wrapping for one text block
 local function wrap(block)
-	--[[
+	---[[
+	local prevnseg = block.nseg or 1
 	local text = block.text
-	local len = #text
+	local tlen = #text
 	local xlen = g_font:getWidth(text)
-	local lmarg = g_font:getWidth(tostring(g_linenum)) + g_lmarg
-	local linew = g_textw - lmarg - g_rmarg
+	local idlen = g_font:getWidth(tostring(block.id))
+	local linew = max(1, g_width - g_lmarg - idlen - g_idgap - g_trgap - g_rmarg)
 	local pos = 1
 	local function nextline()
-		local crem = len + pos - 1 -- characters remaining
-		local clen = math.floor(crem * g_textw / xlen)	-- approx chars per line
-		local n = math.min(clen, crem)
-		local over = (n == crem)
+		local crem = tlen - pos + 1				-- characters remaining
+		local clen = ceil(crem * linew / xlen)	-- approx chars per line
+		local over = false
+		local n = utf8.offset(text, 0, pos + max(1, min(clen, crem))) - pos
+		
+		if n == 0 then n = utf8.offset(text, 2, pos) - pos end
+		--assert(n > 0)
+		--local cnt = 0
 		while true do
 			local t = text:sub(pos, pos + n - 1)
-			local l = g_font:getWidth(t)
+			--local l = g_font:getWidth(t)
+			local ok, l = pcall(g_font.getWidth, g_font, t)
+			if not ok then errorf("pos=%d n=%d t='%s'", pos, n, t) end
 			
-			if l == linew then break		-- exact fit
-			elseif l < linew then			-- too short
-				if over then break end		-- if we have decremented then stop
-				n = n + 1
-			elseif n <= 1 then break		-- text too short?
+			if l == linew then break			-- exact fit
+			elseif l < linew then				-- too short
+				if over then break end			-- if we have decremented then stop
+				if n == crem then break end	-- if end of string then stop
+				n = utf8.offset(text, 2, pos + n) - pos
 			else
+				local p = utf8.offset(text, 0, pos + n - 1) - pos
+				
+				if p == 0 then break end		-- string too short?
+				n = p
 				over = true
-				n = n - 1
 			end
+			--cnt = cnt + 1
+			--if cnt > 99 then
+			--	errorf("tlen=%d xlen=%d done=%d crem=%d clen=%d n=%d over=%s l=%d linew=%d", tlen, xlen, done, crem, clen, n, tostring(over), l, linew)
+			--end
 		end
-		table.insert(block.segs, { pos = pos, last = n < crem and pos + n - 1 or nil })
+		insert(block.segs, { pos = pos, last = n < crem and pos + n - 1 or nil })
 		pos = pos + n
 		return n < crem
 	end
 	
-	block.segs = {}
-	while nextline() do end
-	]]
+	if xlen <= linew then
+		block.nseg = 1
+		block.segs = nil
+	else
+		block.segs = {}
+		while nextline() do end
+		block.nseg = #block.segs
+	end
+	g_linecount = g_linecount + block.nseg - prevnseg
+	--]]
 end
 
 local function lnewline()
 	if #g_screen >= g_maxblocks then
-		table.remove(g_screen, 1)
+		remove(g_screen, 1)
 	end
 	
-	local blocknum = g_blocknum + 1
-	local block = { text = "", num = blocknum }
+	local blockid = g_blockid + 1
+	local block = { text = "", id = blockid }
 	
-	table.insert(g_screen, block)
-	g_blocknum = blocknum
+	insert(g_screen, block)
+	g_blockid = blockid
+	g_linecount = g_linecount + 1
 	
 	return block
 end
 
-local function lprint(...)
-	local t = { ... }
-	
-	for i = 1, #t do t[i] = tostring(t[i]) end
-	
-	local line = table.concat(t, "\t")
-	local block = g_screen[#g_screen]
-	
-	if block then
-		block.text = block.text .. line
-		wrap(block)
-	end
-	
+local function lclear()
+	g_lineref = nil
+	g_blockid = 0		-- ?
+	g_linecount = 0
+	g_screen = {}
 	lnewline()
 end
 
@@ -120,17 +163,32 @@ local function lputch(c)
 	end
 end
 
+local function lprint(...)
+	local t = { ... }
+	
+	for i = 1, #t do t[i] = tostring(t[i]) end
+	
+	local data = concat(t, "\t")
+	local block = g_screen[#g_screen]
+	
+	for line in data:gmatch"[^\n]+" do
+		if block then
+			block.text = block.text .. line
+			wrap(block)
+		end
+		block = lnewline()
+	end
+end
+
 local function lprintf(s, ...)
-	lprint(string.format(s, ...))
+	lprint(format(s, ...))
 end
 
 local function ldump(t, n)
 	local d = ""
 	
 	dump(t, {writer=function(s) d = d .. s end, maxlev=n, sort=true, noadrs=false})
-	if d then
-		for l in d:gmatch"[^\n]+" do lprint(l) end
-	end
+	if #d > 0 then lprint(d) end
 end
 
 local g_shown
@@ -139,11 +197,12 @@ local function lshowinfo()
 	--
 	g_shown = true
 	
-	lprintf("Love2d terminal version 0.2")
+	lprintf("Löve2d terminal version 0.3")
 	lprintf("---------------------------")
 	lprintf("width %d height %d dpiScale %f", g_width, g_height, g_font:getDPIScale())
 	local function show(name)
 		local f = love.filesystem[name]
+		
 		lprintf("love.filesystem.%s: %s", name, f and tostring(f()) or "?")
 	end
 	show"getAppdataDirectory"
@@ -157,34 +216,130 @@ local function lshowinfo()
 end
 
 
-
+-- get current screen parameters
 local function getscreen()
 	local width, height = love.window.getMode()
 	local font = love.graphics.getFont()
 	local dpiScale = font:getDPIScale()
 	
-	return math.floor(width / dpiScale), math.floor(height / dpiScale), font
+	return floor(width / dpiScale), floor(height / dpiScale), font
 end
 	
 -- update screen after resize, font change etc
 local function setscreen()
 	g_width, g_height, g_font = getscreen()
-	
 	g_lineHeight = g_font:getHeight()
-	g_textw, g_texth = g_width, g_height
-	g_nlines = math.floor(g_texth / g_lineHeight)
+	g_nlines = floor(g_height / g_lineHeight)
 
 	for _, blk in ipairs(g_screen) do
 		wrap(blk)
 	end
+	
+	local scrollmax = max(0, g_linecount - g_nlines)
+	
+	g_scroll = g_scroll and min(g_scroll, scrollmax) or nil
+	g_lineref = nil
 end
 
+-- check whether screen parameters have changed
 local function chkscreen()
 	local width, height, font = getscreen()
 
 	return width ~= g_width or	height ~= g_height
 end
 
+
+-- find block and segment for line
+local function getline(line)
+	local ref = g_lineref
+	
+	-- check for no change
+	if ref and ref.line == line then return ref.blknum, ref.segnum end
+		
+	local function getref()
+		local first, last
+		
+		-- check for different line in same block, otherwise set upper/lower bound for search
+		if ref then
+			local block = ref.block
+			local nsegs = block.nseg or 1
+			
+			if line < ref.line then
+				if ref.line - line < ref.segnum then return block, ref.blknum, ref.segnum - (ref.line - line) end
+				last = ref
+			else
+				if line - ref.line <= nsegs - ref.segnum then return block, ref.blknum, ref.segnum + (line - ref.line) end
+				first = ref
+			end
+		end
+		
+		first = first or { line = 0, blknum = 1, segnum = 1, block = g_screen[1] }
+		last = last or { line = g_linecount - (g_screen[#g_screen].nseg or 1), blknum = #g_screen, segnum = 1, block = g_screen[#g_screen] }
+		
+		-- determine which end is closer to desired line number
+		if line - first.line < last.line - line then
+			-- scan forwards from first
+			local blknum = first.blknum
+			local dist = line - first.line + first.segnum - 1
+			
+			while blknum <= #g_screen do
+				local block = g_screen[blknum]
+				local nsegs = block.nseg or 1
+			
+				if dist < nsegs then return block, blknum, dist + 1 end
+				blknum = blknum + 1
+				dist = dist - nsegs
+			end
+			errorf("line %d dist %d first.line %d last.line %d blknum %d #g_screen %d", line, dist, first.line, last.line, blknum, #g_screen)
+		else
+			-- scan backwards from last
+			local blknum = last.blknum
+			local dist = line - last.line + last.segnum - 1
+			local block = g_screen[blknum]
+			
+			while blknum > 0 do
+				if dist >= 0 then return block, blknum, dist + 1 end
+				blknum = blknum - 1
+				block = g_screen[blknum]
+				dist = dist + (block.nseg or 1)
+			end
+			errorf("line %d dist %d first.line %d last.line %d blknum %d #g_screen %d", line, dist, first.line, last.line, blknum, #g_screen)
+		end
+	end
+	local block, blknum, segnum = getref()
+	
+	g_lineref = { line = line, blknum = blknum, segnum = segnum, block = block }
+	
+	return blknum, segnum
+end
+
+-- update screen
+local function drawscreen(startline, count, drawfunc)
+	local blknum, segnum = getline(startline)
+	
+	while blknum <= #g_screen do
+		local block = g_screen[blknum]
+		local text = block.text
+		local nsegs = block.nseg or 1
+		local segments = block.segs or {{ pos = 1 }}
+		
+		while segnum <= nsegs do
+			local seg = segments[segnum]
+			--if not seg then errorf("blknum %d segnum %d nsegs %d count %d", blknum, segnum, nsegs, count) end
+			local line = text:sub(seg.pos, seg.last)
+			
+			drawfunc(line, block.id, segnum == 1)
+			
+			count = count - 1
+			if count <= 0 then return end
+			segnum = segnum + 1
+		end
+		blknum = blknum + 1
+		segnum = 1
+	end
+end
+
+	
 
 -- screen update & mouse handling
 ---------------------------------
@@ -198,25 +353,8 @@ local function ldraw( )
 	--y = y + 20
 	if chkscreen() then setscreen() end
 	if not g_shown then lshowinfo() end
-	
---[[	
-	local blk, seg = 1, 1
-	if g_scroll then
-		local rem = g_scroll
-		
-		for b, block in ipairs(g_screen) do
-			if #block.segs > rem then
-				blk = b
-				break
-			end
-			rem = rem - #block.segs
-		end
-		seg = rem + 1
-	else
-		-- display last lines
-		for b = #g_screen, 1, -1 do
-			local block = g_screen[b]
-	--]]		
+	--if not g_first then return end
+	--g_first = nil
 	
 	-- mouse handling
 	local mactive = true
@@ -230,7 +368,7 @@ local function ldraw( )
 	local press = mdown and not g_pdown
 	local reles = not mdown and g_pdown
 	local bound = g_pvid and 0 or 2
-	local moved = math.abs(dx) > bound or math.abs(dy) > bound
+	local moved = abs(dx) > bound or abs(dy) > bound
 	local mvid = mdown and g_pvid or nil
 	
 	if press then
@@ -243,31 +381,36 @@ local function ldraw( )
 	love.graphics.clear(1, 1, 0.8)
 	
 	-- effective scroll position
-	local scrollmax = math.max(0, #g_screen - g_nlines)
+	local scrollmax = max(0, g_linecount - g_nlines)
 	local scroll = g_scroll or scrollmax
 	
 	-- display text
 	local x, y = g_lmarg, 0
-	
-	for i = scroll + 1, #g_screen do
-		local blk = g_screen[i]
+	local function drawline(text, ident, first)
+		local idstr = tostring(ident)
+		local idlen = g_font:getWidth(idstr)
 		
-		if not blk then break end
-		
-		local lineno = tostring(blk.num)
-		local linenlen = g_font:getWidth(lineno)
-		
-		love.graphics.setColor(1, 0, 0)
-		love.graphics.print(lineno, x, y)
+		if first then
+			love.graphics.setColor(1, 0, 0)
+			love.graphics.print(idstr, x, y)
+		end
 		love.graphics.setColor(0, 0, 0)
-		love.graphics.print(blk.text, x + linenlen + 2, y)
+		love.graphics.print(text, x + idlen + g_idgap, y)
+		
 		y = y + g_lineHeight
 	end
+	drawscreen(scroll, g_nlines, drawline)
 	
 	-- display scroll bar & buttons; handle mouse operations
 	local bheight = g_rmarg
 	local bwidth = g_rmarg
 	local xbar = g_width - bwidth
+	
+	local hheight = 20
+	local sheight = g_height - bheight * 3 - hheight
+	local spos = bheight * 2
+	local hpos = round(sheight * (g_scroll and scroll / max(1, scrollmax) or 1))
+	local htop = spos + round(hheight / 2)
 	
 	local function button(b)
 		
@@ -289,8 +432,10 @@ local function ldraw( )
 	end
 	
 	
-	love.graphics.setColor(0.7, 0.7, 0.7, 1)
-	love.graphics.rectangle("fill", xbar, 0, g_rmarg, g_height)
+	love.graphics.setColor(0.7, 0.7, 0.7)
+	love.graphics.rectangle("fill", xbar, 0, bwidth, g_height)
+	love.graphics.setColor(0.4, 0.4, 0.4)
+	love.graphics.rectangle("fill", xbar + (bwidth - 2) / 2, htop, 2, sheight)
 	
 	local button1 = { id = 1, x = xbar, y = 0, w = bwidth, h = bheight, colour = { 0.8, 0.3, 0.3 },
 		onclick = function()
@@ -299,7 +444,7 @@ local function ldraw( )
 		end
 	}
 	local button2 = { id = 2, x = xbar, y = bheight, w = bwidth, h = bheight, colour = { 0.3, 0.3, 0.8 }, onclick = function()
-			g_scroll = math.max(0, scroll - 1)
+			g_scroll = max(0, scroll - 1)
 		end
 	}
 	local button3 = { id = 3, x = xbar, y = g_height - bheight, w = bwidth, h = bheight, colour = { 0.3, 0.3, 0.8 },
@@ -314,10 +459,6 @@ local function ldraw( )
 	button(button2)
 	button(button3)
 	
-	local hheight = 20
-	local sheight = g_height - bheight * 3 - hheight
-	local hpos = math.floor(0.5 + sheight * scroll / math.max(1, #g_screen - g_nlines))
-	local htop = bheight * 2 + hheight / 2
 	local handle = { id = 4, x = xbar, y = bheight * 2 + hpos, w = bwidth, h = hheight, colour = { 0.2, 0.2, 0.2 },
 		onpress = function(b)
 			--lprintf("press handle")
@@ -325,7 +466,7 @@ local function ldraw( )
 		end,
 		onmove = function(b)
 			--lprintf("move handle")
-			scroll = math.max(0, math.min(scrollmax, math.floor(0.5 + scrollmax * (my - htop) / sheight)))
+			scroll = max(0, min(scrollmax, round(scrollmax * (my - htop) / sheight)))
 			g_scroll = scroll < scrollmax and scroll or nil
 		end
 	}
@@ -338,13 +479,21 @@ local function ldraw( )
 	return mactive
 end
 
+local function lexit()
+	g_pactive = false
+end
+
+lclear()
 setscreen()
 
+
 terminal.draw = ldraw
+terminal.exit = lexit
+terminal.putch = lputch
 terminal.print = lprint
 terminal.printf = lprintf
 terminal.dump = ldump
-terminal.putch = lputch
 terminal.showinfo = lshowinfo
+terminal.clear = lclear
 
 return terminal
